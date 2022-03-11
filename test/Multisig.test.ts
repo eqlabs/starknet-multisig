497a634b747bc2e5e72faaa08e8901500dbf7b97
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-03-11 09:43:13 +0200

    Upgraded starknet-hardhat plugin

--- test/Multisig.test.ts
+++ test/Multisig.test.ts
@@ -17,7 +17,7 @@ describe("Starknet", function () {
   let publicKey: string;
 
   before(async function() {
-    account = await starknet.deployAccountFromABI("Account", "OpenZeppelin");
+    account = await starknet.deployAccount("OpenZeppelin");
     accountAddress = account.starknetContract.address;
     privateKey = account.privateKey;
     publicKey = account.publicKey;
@@ -32,9 +32,7 @@ describe("Starknet", function () {
     console.log("Started deployment");
     contract = await contractFactory.deploy({ initial_balance: 0 });
     
-    console.log("Deployed at", contract.address);
-
-
+    console.log("Deployed target contract at", contract.address);
     console.log("Deployed account at address:", account.starknetContract.address);
     console.log("Private and public key:", privateKey, publicKey);
   });
@@ -47,58 +45,9 @@ describe("Starknet", function () {
       calldata: [5]
     };
     await account.invoke(multisig, "submit_transaction", payload);
-    await account.invoke(multisig, "get_transactions_len", {});
 
     await account.invoke(multisig, "confirm_transaction", { tx_index: 0n });
     console.log('starting execute');
     const res3 = await account.invoke(multisig, "execute_transaction", { tx_index: 0n });
   });
-
-  
-
-/* 
-  it("should load an already deployed account with the correct private key", async function() {
-
-    const loadedAccount = await starknet.getAccountFromAddress("Account", accountAddress, privateKey, "OpenZeppelin");
-
-    expect(loadedAccount.starknetContract.address).to.deep.equal(accountAddress);
-    expect(loadedAccount.privateKey).to.deep.equal(privateKey);
-    expect(loadedAccount.publicKey).to.deep.equal(publicKey);
-  });
-
-  it("should fail when loading an already deployed account with a wrong private key", async function() {
-    try{
-      await starknet.getAccountFromAddress("Account" , accountAddress, "0x0123", "OpenZeppelin");
-      expect.fail("Should have failed on passing an incorrect private key.");
-    } catch(err: any) {
-      expect(err.message).to.equal("The provided private key is not compatible with the public key stored in the contract.");
-    }
-  });
-
-  it("should invoke a function on another contract", async function() {
-    const { res: currBalance } = await account.call(contract, "get_balance");
-    const amount1 = 10n;
-    const amount2 = 20n;
-    await account.invoke(contract, "increase_balance", { amount1, amount2 });
-
-    const { res: newBalance } = await account.call(contract, "get_balance");
-    expect(newBalance).to.deep.equal(currBalance + amount1 + amount2);
-  });
-
-  it("should work with arrays through an account", async function() {
-    const { res } = await account.call(contract, "sum_array", { a: [1, 2, 3] });
-    expect(res).to.equal(6n);
-  });
-
-  it("should work with BigNumbers and tuples through an account", async function() {
-    // passing Points (1, 2) and (3, 4) in a tuple
-    const { res: sum } = await account.call(contract, "sum_points_to_tuple", {
-      points: [
-        { x: BigNumber.from(1), y: BigNumber.from(2) },
-        { x: 3, y: 4 }
-      ]
-    });
-    expect(sum).to.deep.equal([4n, 6n]);
-  }); */
-
 });
