9f91cb371794a26711c56fbc7e753da63bb43604
edwin <edwinswatpako@gmail.com>
2022-07-04 10:02:47 +0300

    Added utils and tests for unique_elements

---
+++ contracts/test/utils.ts
@@ -0,0 +1,33 @@
+import { expect } from "chai";
+import { number } from "starknet";
+import { getSelectorFromName } from "starknet/dist/utils/hash";
+import { BigNumber, utils } from "ethers";
+
+export const defaultPayload = (contractAddress: string, newValue: number) => {
+  const setSelector = number.toBN(getSelectorFromName("set_balance"));
+  const target = number.toBN(contractAddress);
+  const setPayload = {
+    to: target,
+    function_selector: setSelector,
+    calldata: [newValue],
+  };
+  return setPayload;
+};
+
+export const assertErrorMsg = (full: string, expected: string) => {
+  expect(full).to.deep.contain("Transaction rejected. Error message:");
+  const match = /Error message: (.+?)\n/.exec(full);
+  if (match && match.length > 1) {
+    expect(match[1]).to.equal(expected);
+    return;
+  }
+  expect.fail("No expected error found: " + expected);
+};
+
+export function randomIntegerFromRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export function randomBigNumber() {
+  return BigNumber.from(utils.randomBytes(31));
+}
